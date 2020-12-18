1de9dc55c45d3c5551643aa8267fe6fcea166709
Joliver Estampador <joliverestampador@gmail.com>
2020-12-18 13:23:22 +0800

    removing duplicates

--- tdd-by-example-master/src/main/java/guru/springframework/Dollar.java
+++ tdd-by-example-master/src/main/java/guru/springframework/Dollar.java
@@ -1,20 +1,11 @@
 package guru.springframework;
 
-public class Dollar {
-
-    private int amount;
-
+public class Dollar extends Money{
     public Dollar(int amount) {
         this.amount = amount;
     }
 
     Dollar times(int multiplier) {
         return new Dollar(multiplier * amount);
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        Dollar dollar = (Dollar) o;
-        return dollar.amount == amount;
     }
 }
--- tdd-by-example-master/src/main/java/guru/springframework/Franc.java
+++ tdd-by-example-master/src/main/java/guru/springframework/Franc.java
@@ -1,20 +1,11 @@
 package guru.springframework;
 
-public class Franc {
-
-    private int amount;
-
+public class Franc extends Money {
     public Franc(int amount) {
         this.amount = amount;
     }
 
     Franc times(int multiplier) {
         return new Franc(multiplier * amount);
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        Franc franc = (Franc) o;
-        return franc.amount == amount;
     }
 }
